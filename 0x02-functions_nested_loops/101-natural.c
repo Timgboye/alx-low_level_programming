6c4c0147ae929813852acb46c21491226a0fe02a
Timothy <timgboye@gmail.com>
2023-03-22 11:37:59 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, sum;
+
+	sum = 0;
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+		{
+			sum = sum + i;
+		}
+		else
+		{
+			continue;
+		}
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
