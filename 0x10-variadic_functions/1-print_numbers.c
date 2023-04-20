c9e05aeafd6d6b0da6953eba2bd9bca9da71d2c8
Timothy <timgboye@gmail.com>
2023-04-20 12:11:04 -0700

    Task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print integers
+ * @n: number of integers to print
+ * @separator: string to be printed between numbers.
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i, arr;
+
+	/* declare variadic datatypes pointer*/
+	va_list ptr_n;
+
+	/* start function */
+	va_start(ptr_n, n);
+
+	for (i = 0; i < n; i++)
+	{
+		arr = va_arg(ptr_n, const unsigned int);
+		printf("%d", arr);
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ptr_n);
+}
